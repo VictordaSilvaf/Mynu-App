4851ae53703d1e266e9820d3e020f570ccf77f77
VictordaSilvaf <victordasilvafernandes@gmail.com>
2026-01-21 13:11:50 -0300

    feat(user): atribuir papel 'free' ao criar novo usuário

--- app/Actions/Fortify/CreateNewUser.php
+++ app/Actions/Fortify/CreateNewUser.php
@@ -30,10 +30,14 @@ class CreateNewUser implements CreatesNewUsers
             'password' => $this->passwordRules(),
         ])->validate();
 
-        return User::create([
+        $newUser = User::create([
             'name' => $input['name'],
             'email' => $input['email'],
             'password' => $input['password'],
         ]);
+
+        $newUser->assignRole('free');
+
+        return $newUser;
     }
 }
